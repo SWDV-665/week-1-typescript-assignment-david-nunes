7eb62928ee688ebd49012499c65b6942dc9ab7ba
David Nunes <96022446+david-nunes@users.noreply.github.com>
2022-07-01 14:43:14 -0400

    Added price to Grocery class

--- grocery.js
+++ grocery.js
@@ -1,21 +1,23 @@
 var Grocery = /** @class */ (function () {
-    function Grocery(name, quantity) {
+    function Grocery(name, quantity, price) {
         this.name = name;
         this.quantity = quantity;
+        this.price = price;
     }
     return Grocery;
 }());
-var groceryArray = [["milk", 25], ["bread", 37], ["eggs", 56]];
+var groceryArray = [["milk", 25, 3.99], ["bread", 37, 2.99], ["eggs", 56, 5.99]];
 var groceryObjects = [];
 for (var _i = 0, groceryArray_1 = groceryArray; _i < groceryArray_1.length; _i++) {
     var entry = groceryArray_1[_i];
-    var newObj = new Grocery(entry[0], entry[1]);
+    var newObj = new Grocery(entry[0], entry[1], entry[2]);
     groceryObjects.push(newObj);
 }
 for (var _a = 0, groceryObjects_1 = groceryObjects; _a < groceryObjects_1.length; _a++) {
     var obj = groceryObjects_1[_a];
     var name_1 = obj["name"];
     var quantity = obj["quantity"];
-    var htmlString = "<li><h3>".concat(name_1, "</h3><p>Quantity: ").concat(quantity, "</p></li>");
+    var price = obj["price"];
+    var htmlString = "<li><h3>".concat(name_1, "</h3><p>Quantity: ").concat(quantity, " Price: $").concat(price, "</p></li>");
     document.getElementById("root").innerHTML += htmlString;
 }
--- grocery.ts
+++ grocery.ts
@@ -1,23 +1,26 @@
 class Grocery {
     name: string;
     quantity: number;
-    constructor(name, quantity) {
+    price: number;
+    constructor(name, quantity, price) {
         this.name = name;
         this.quantity = quantity;
+        this.price = price;
     }
 }
 
-let groceryArray: Array<any> = [["milk", 25], ["bread", 37], ["eggs", 56]];
+let groceryArray: Array<any> = [["milk", 25, 3.99], ["bread", 37, 2.99], ["eggs", 56, 5.99]];
 let groceryObjects: Array<object> = [];
 
 for (const entry of groceryArray) {
-    const newObj: Grocery = new Grocery(entry[0], entry[1]);
+    const newObj: Grocery = new Grocery(entry[0], entry[1], entry[2]);
     groceryObjects.push(newObj);
 }
 
 for (const obj of groceryObjects){
     let name: string = obj["name"];
     let quantity: number = obj["quantity"];
-    let htmlString: string = `<li><h3>${name}</h3><p>Quantity: ${quantity}</p></li>`;
+    let price: number = obj["price"]
+    let htmlString: string = `<li><h3>${name}</h3><p>Quantity: ${quantity} Price: $${price}</p></li>`;
     document.getElementById("root").innerHTML += htmlString;
 }
